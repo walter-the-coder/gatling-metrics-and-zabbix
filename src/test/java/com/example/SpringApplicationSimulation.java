9aa7237db60b7f359f371dd4fb1b0ae151e5e295
walter-the-coder <7272352+walter-the-coder@users.noreply.github.com>
2024-10-02 14:00:08 +0200

    Ramping users

--- src/test/java/com/example/SpringApplicationSimulation.java
+++ src/test/java/com/example/SpringApplicationSimulation.java
@@ -3,7 +3,7 @@ package com.example;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
-import static io.gatling.javaapi.core.OpenInjectionStep.atOnceUsers;
+import static io.gatling.javaapi.core.CoreDsl.rampUsers;
 import static io.gatling.javaapi.http.HttpDsl.http;
 
 public class SpringApplicationSimulation extends Simulation {
@@ -14,9 +14,9 @@ public class SpringApplicationSimulation extends Simulation {
     {
         setUp(
             new ReceptionControllerSimulation().scenarioBuilder()
-                .injectOpen(atOnceUsers(1)),
+                .injectOpen(rampUsers(1200).during(600)),
             new RetrievingControllerSimulation().scenarioBuilder()
-                .injectOpen(atOnceUsers(1))
+                .injectOpen(rampUsers(120).during(600))
         ).protocols(httpProtocol);
     }
 }
